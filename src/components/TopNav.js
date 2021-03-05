fdc8e68ffb925f96b19819eba4ab383ebeda4ee6
mounikasantoshi <mounikasantoshi99@gmail.com>
2021-03-06 00:05:25 +0530

    product

---
+++ src/components/TopNav.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import data from "./../data.json";
+import Products from "./Products";
+
+export class TopNav extends Component {
+  state = {
+    products: data.products,
+    size: "",
+    sort: "",
+  };
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">ShopingCart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All right is reserved</footer>
+      </div>
+    );
+  }
+}
+
+export default TopNav;
