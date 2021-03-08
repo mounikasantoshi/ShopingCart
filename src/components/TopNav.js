beeb1d1e1e23e2333ac656ced57f3986083aff3d
mounikasantoshi <mounikasantoshi99@gmail.com>
2021-03-08 23:50:51 +0530

    filter

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import data from "./../data.json";
+import Filter from "./Filter";
 import Products from "./Products";
 
 export class TopNav extends Component {
@@ -7,6 +8,42 @@ export class TopNav extends Component {
     products: data.products,
     size: "",
     sort: "",
+  };
+  sortProducts = (e) => {
+    // console.log(e.target.value);
+    const sort = e.target.value;
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id > b._id
+            ? 1
+            : -1
+        ),
+    }));
+  };
+
+  filterProducts = (e) => {
+    // console.log(e.target.value);
+    if (e.target.value === "") {
+      this.setState({ size: e.target.value, products: data.products });
+    } else {
+      this.setState({
+        size: e.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(e.target.value) >= 0
+        ),
+      });
+    }
   };
   render() {
     return (
@@ -17,6 +54,13 @@ export class TopNav extends Component {
         <main>
           <div className="content">
             <div className="main">
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              />
               <Products products={this.state.products} />
             </div>
             <div className="sidebar">Cart Items</div>
